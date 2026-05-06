db45939be5c87c4fae9d626a1a7b7e37f55b6534
Ameen <your-email@example.com>
2026-05-06 14:59:35 +0530

    ameen mamu

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,6 @@
-#!/bin/badsh
+#!/bin/bash
 
-echo"Ameen:: hii mamu how are you?"
-echo"mamu::hii ameen i am fine how are you?"
-echo"ameen::what you are leaning?"
-echo"mamu:: i'm learning aws devops"
+echo "Ameen:: hii mamu how are you?"
+echo "mamu:: hii ameen i am fine how are you?"
+echo "ameen:: what you are learning?"
+echo "mamu:: I'm learning AWS DevOps"
---
+++ 04-variables.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Enter PIN_NUMBER:"
+read $PIN_NUMBER
+echo "Entered number is:$PIN_NUMBER"
