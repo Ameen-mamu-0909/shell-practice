e5ec2e30e9d0a1c6c973c6193d8ad1ff5c9b0d10
Ameen <your-email@example.com>
2026-05-07 12:16:21 +0530

    mamu

--- 09-condition.sh
+++ 09-condition.sh
@@ -5,9 +5,14 @@ read NUMBER
 
 if [ $((NUMBER % 2)) -eq 0 ]; then
     echo "given number $NUMBER is even"
-else 
+else
     echo "given number $NUMBER is odd"
-fi 
+fi
 
 yum install mysql -y
-echo $?
+
+if [ $? -eq 0 ]; then
+  echo "installation mysql is successful"
+else
+  echo "installation myswl if failure"
+  fi   
