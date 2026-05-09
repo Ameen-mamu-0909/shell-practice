818430d661b45f8bff575076f82f49411eab5abc
Ameen <your-email@example.com>
2026-05-09 11:29:53 +0530

    ameen

--- 13-practice.sh
+++ 13-practice.sh
@@ -6,7 +6,7 @@ USERID=$(id -u)
 
 yum install mysql -y
 
-if [ $? -eq 0 ]; then
+if [ $? -ne 0 ]; then
  echo "mysql installation successfully....&G" | tee -a logs.txt
 else
  echo "mysql installation failure...&R" | tee -a logs.txt
