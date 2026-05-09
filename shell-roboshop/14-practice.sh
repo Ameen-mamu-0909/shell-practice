43b2ed1c0be9a1bd018f1615ee1a16ab5260362b
Ameen <your-email@example.com>
2026-05-09 11:36:11 +0530

    ameen

---
+++ shell-roboshop/14-practice.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+LOGS_FILE="logs.txt"
+USERID=$(id -u)
+
+
+yum install mysql -y
+stauts=$?
+
+if [ $? -ne 0 ]; then
+ echo "mysql installation successfully....&G" | tee -a logs.txt
+else
+ echo "mysql installation failure...&R" | tee -a logs.txt
+fi 
