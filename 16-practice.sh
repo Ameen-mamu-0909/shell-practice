94ab267e82cc1ef044c7de3e22fa124c51edb6a2
Ameen <your-email@example.com>
2026-05-09 16:15:22 +0530

    ameen

--- 16-practice.sh
+++ 16-practice.sh
@@ -2,25 +2,23 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]; then
-   echo "please run with root access"
-exit 1
+if [ $USERID -ne 0 ]
+then
+    echo "Please run with root access"
+    exit 1
+fi
 
 dnf install httpd -y
 
-if [ $? -eq 0 ]; then
-    echo "Apache Installation is success..."
-esle 
-    echo "Apache Iinstallation is failure.."
+if [ $? -eq 0 ]
+then
+    echo "Apache Installed Successfully"
+else
+    echo "Apache Installation Failed"
     exit 1
 fi
- 
- systemctl enable httpd
- systemctl start httpd
 
- systemctl status httpd
+systemctl enable httpd
+systemctl start httpd
 
-
-
-
-
+systemctl status httpd
