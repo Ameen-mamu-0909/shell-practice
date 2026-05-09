5cdaae40a6038c6a681c4ec06d3de4c6b759a08f
Ameen <your-email@example.com>
2026-05-09 11:20:49 +0530

    ameen

---
+++ 13-practice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+USERID=$(id -u)
+
+yum install mysql -y
+
+if [ $? -eq 0]; then
+ echo "mysql installation successfully....&G"
+else
+ echo "mysql installation failure...&R"
+fi 
