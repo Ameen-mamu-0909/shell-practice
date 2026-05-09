cd987b5ba00f4426ef8ba483c767a64f553c2b1e
Ameen <your-email@example.com>
2026-05-09 14:41:09 +0530

    ameen

--- 14.practice.sh
+++ 14.practice.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-
+R="\e[31m"
+G="\e[31m"
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
-   echo "your are root user"
+   echo "your are root user....$G"
 else 
-   echo "your are normal user"
+   echo "your are normal user.....$R"
 fi   
