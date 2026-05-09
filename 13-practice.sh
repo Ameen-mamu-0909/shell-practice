fe653e00d4ff6167ee4e131745525d67a499b5f8
Ameen <your-email@example.com>
2026-05-09 14:30:23 +0530

    ameen

--- 13-practice.sh
+++ 13-practice.sh
@@ -5,11 +5,19 @@ G="\e[32m"
 USERID=&(id -u)
 LOGS_FILE="/tmp/installation.log"
 
-yum remove mysql-server -y
-yum install mysql -y &&>> $LOGS_FILE
+if [ $USERID -ne 0 ];then
+ echo "please run this code root::privilage'
+exit 1
 
-if [ $? -ne 0 ]; then
- echo "mysql installation successfully....&G" | tee -a $LOGS_FILE
-else
- echo "mysql installation failure...&R" | tee -a $LOGS_FILE
+VALIDATE () {
+         if [ $? -eq 0 ]; then
+             echo "mysql installation successfully....&G" | tee -a $LOGS_FILE
+        else
+             echo "mysql installation failure...&R" | tee -a $LOGS_FILE
 fi 
+}
+
+yum remove mysql-server -y &&>> $LOGS_FILE
+VALIDATE $? "removing mysql server"
+yum install mysql -y &&>> $LOGS_FILE
+VALIDATE $? "installing mysql server"
